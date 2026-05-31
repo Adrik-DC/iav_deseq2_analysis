8425a3298ba961d12dd09132a5b9ddb620f5b8d6
Adrik-DC <adrikDC@outlook.com>
2026-05-31 01:35:41 -0600

    feat: leer, filtrar y agregar informacion de archivo gff

--- analyse_iav.py
+++ analyse_iav.py
@@ -27,6 +27,8 @@ def parsear_argumentos():
 
     parser.add_argument("-o", "--output", help="Ruta al archivo fasta de salida")
 
+    parser.add_argument("-g", "--gff-file", help="Ruta al archivo GFF de entrada")
+
     parser.add_argument(
         "--lfc-threshold",
         type=float,
@@ -116,7 +118,141 @@ def load_deseq2_results(filename, gene_col, lfc_col, padj_col):
             log2FoldChange = fields[lfc_col - 1]
             padj = fields[padj_col - 1]
             interactions.append((gene, log2FoldChange, padj))
-    return interactions
+    return sorted(interactions)
+
+
+# ------------------------------------------
+# Extraer la columna de atributos del archivo GFF
+# ------------------------------------------
+# ------------------------------------------
+# Responsabilidad: Extraer la columna de atributos del archivo GFF
+# Entrada: gff_file
+# Salida: gff_data (columna de atributos del archivo GFF)
+# ------------------------------------------
+
+
+def load_gff(gff_file):
+    """Carga la columna de atributos de un archivo GFF.
+
+    Lee un archivo GFF y extrae la columna de atributos (campo 9) de cada
+    línea no comentada.
+
+    Args:
+        gff_file (str): Ruta al archivo GFF de entrada.
+
+    Returns:
+        list[str]: Lista de cadenas con la columna de atributos de cada línea.
+
+    Raises:
+        SystemExit: Si el archivo no existe.
+    """
+
+    gff_data = []
+    if not os.path.exists(gff_file):
+        print("Error: archivo no encontrado")
+        exit(1)
+    index = 9
+    with open(gff_file) as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            fields = line.strip().split("\t")
+            if len(fields) < index:
+                continue
+            attributes = fields[8]
+            gff_data.append(attributes)
+    return gff_data
+
+
+# ------------------------------------------
+# Extraer los datos significativos del archivo GFF
+# ------------------------------------------
+# ------------------------------------------
+# Responsabilidad: Extraer los datos significativos del archivo GFF
+# Entrada: gff_data
+# Salida: Informacion significativa del archivo GFF ordenada
+# ------------------------------------------
+
+
+def save_gff_data(gff_data):
+    """Extrae campos significativos de la columna de atributos del GFF.
+
+    Busca atributos `ID`, `Name`, `description` y `gene_type` y devuelve una
+    lista de tuplas ordenadas con (gene_id, gene_name, description, gene_type).
+
+    Args:
+        gff_data (iterable): Iterable de cadenas (atributos) extraídas del GFF.
+
+    Returns:
+        list[tuple[str, str, str, str]]: Lista ordenada de tuplas con los campos.
+    """
+
+    all_data = []
+    for attributes in gff_data:
+        if (
+            "ID" in attributes
+            and "Name" in attributes
+            and "description" in attributes
+            and "gene_type" in attributes
+        ):
+            gene_id = attributes.split("ID=")[1].split(";")[0]
+            gene_name = attributes.split("Name=")[1].split(";")[0]
+            description = attributes.split("description=")[1].split(";")[0]
+            gene_type = attributes.split("gene_type=")[1].split(";")[0]
+            all_data.append(
+                (
+                    gene_id,
+                    gene_name,
+                    description,
+                    gene_type,
+                )
+            )
+    return sorted(all_data)
+
+
+# ------------------------------------------
+# Combinar los datos del archivo de entrada con los datos del archivo GFF
+# ------------------------------------------
+# ------------------------------------------
+# Responsabilidad: Combinar los datos del archivo de entrada con los datos del archivo GFF
+# Entrada: genes_tupla, gff_significant_data
+# Salida: merged_data (datos combinados)
+# ------------------------------------------
+
+
+def merge_data(genes_tupla, gff_significant_data):
+    """Combina resultados de DESeq2 con datos significativos del GFF.
+
+    Busca coincidencias entre el nombre del gen en `genes_tupla` y `gene_name`
+    en `gff_significant_data` y produce una lista de tuplas combinadas.
+
+    Args:
+        genes_tupla (iterable): Iterable de tuplas (gene, log2FoldChange, padj).
+        gff_significant_data (iterable): Iterable de tuplas
+            (gene_id, gene_name, description, gene_type).
+
+    Returns:
+        list[tuple]: Lista de tuplas combinadas
+            (gene, log2FoldChange, padj, gene_id, gene_name, description, gene_type).
+    """
+
+    merged = []
+    for gene, log2FoldChange, padj in genes_tupla:
+        for gene_id, gene_name, description, gene_type in gff_significant_data:
+            if gene == gene_name:
+                merged.append(
+                    (
+                        gene,
+                        log2FoldChange,
+                        padj,
+                        gene_id,
+                        gene_name,
+                        description,
+                        gene_type,
+                    )
+                )
+                break
+    return merged
 
 
 # ------------------------------------------
@@ -129,22 +265,32 @@ def load_deseq2_results(filename, gene_col, lfc_col, padj_col):
 # ------------------------------------------
 
 
-def is_significant(genes_tupla, padj_threshold, lfc_threshold):
+def is_significant(merged_data, padj_threshold, lfc_threshold):
     """Filtra genes significativos según umbrales de padj y log2FoldChange.
 
     Args:
-        genes_tupla (iterable): Iterador o lista de tuplas `(gene, log2FoldChange, padj)`.
+        merged_data (iterable): Iterador o lista de tuplas `(gene, log2FoldChange, padj, gene_id, gene_name, description, gene_type)`.
         padj_threshold (float): Umbral máximo para `padj` (valor < padj_threshold).
         lfc_threshold (float): Umbral mínimo absoluto para `log2FoldChange`.
 
     Returns:
-        list[tuple[str, str, str]]: Subconjunto de `genes_tupla` que cumplen los umbrales.
+        list[tuple[str, str, str, str, str, str, str]]: Subconjunto de `merged_data` que cumplen los umbrales.
     """
 
     filtro = []
-    for gene, log2FoldChange, padj in genes_tupla:
+    for (
+        gene,
+        log2FoldChange,
+        padj,
+        gene_id,
+        gene_name,
+        description,
+        gene_type,
+    ) in merged_data:
         if abs(float(log2FoldChange)) >= lfc_threshold and float(padj) < padj_threshold:
-            filtro.append((gene, log2FoldChange, padj))
+            filtro.append(
+                (gene, log2FoldChange, padj, gene_id, gene_name, description, gene_type)
+            )
     return filtro
 
 
@@ -173,13 +319,54 @@ def classify_gene(filtro):
     """
 
     classify = []
-    for gene, log2FoldChange, padj in filtro:
+    for (
+        gene,
+        log2FoldChange,
+        padj,
+        gene_id,
+        gene_name,
+        description,
+        gene_type,
+    ) in filtro:
         if float(log2FoldChange) >= 1:
-            classify.append((gene, log2FoldChange, padj, "upregulated"))
+            classify.append(
+                (
+                    gene,
+                    log2FoldChange,
+                    padj,
+                    gene_id,
+                    gene_name,
+                    description,
+                    gene_type,
+                    "upregulated",
+                )
+            )
         elif float(log2FoldChange) <= -1:
-            classify.append((gene, log2FoldChange, padj, "downregulated"))
+            classify.append(
+                (
+                    gene,
+                    log2FoldChange,
+                    padj,
+                    gene_id,
+                    gene_name,
+                    description,
+                    gene_type,
+                    "downregulated",
+                )
+            )
         else:
-            classify.append((gene, log2FoldChange, padj, "not_significant"))
+            classify.append(
+                (
+                    gene,
+                    log2FoldChange,
+                    padj,
+                    gene_id,
+                    gene_name,
+                    description,
+                    gene_type,
+                    "not_significant",
+                )
+            )
     return classify
 
 
@@ -198,16 +385,29 @@ def write_results(output, classify):
 
     Args:
         output (str): Ruta del archivo de salida donde se escribirán los resultados.
-        classify (iterable): Lista de tuplas `(gene, log2FoldChange, padj, classification)`.
+        classify (iterable): Lista de tuplas `(gene, log2FoldChange, padj, gene_id, gene_name, description, gene_type, classification)`.
 
     Returns:
         None
     """
 
     with open(output, "w") as f:
-        f.write("Gene\tlog2FoldChange\tpadj\tClassification\n")
-        for gene, log2FoldChange, padj, classification in classify:
-            f.write(f"{gene}\t{log2FoldChange}\t{padj}\t{classification}\n")
+        f.write(
+            "Gene\tlog2FoldChange\tpadj\tGene_ID\tGene_Name\tDescription\tGene_Type\tClassification\n"
+        )
+        for (
+            gene,
+            log2FoldChange,
+            padj,
+            gene_id,
+            gene_name,
+            description,
+            gene_type,
+            classification,
+        ) in classify:
+            f.write(
+                f"{gene}\t{log2FoldChange}\t{padj}\t{gene_id}\t{gene_name}\t{description}\t{gene_type}\t{classification}\n"
+            )
     print(f"Resultados escritos en {output}")
 
 
@@ -232,13 +432,19 @@ def print_summary(classify):
     """
 
     upregulated = sum(
-        1 for _, _, _, classification in classify if classification == "upregulated"
+        1
+        for _, _, _, _, _, _, _, classification in classify
+        if classification == "upregulated"
     )
     downregulated = sum(
-        1 for _, _, _, classification in classify if classification == "downregulated"
+        1
+        for _, _, _, _, _, _, _, classification in classify
+        if classification == "downregulated"
     )
     not_significant = sum(
-        1 for _, _, _, classification in classify if classification == "not_significant"
+        1
+        for _, _, _, _, _, _, _, classification in classify
+        if classification == "not_significant"
     )
     print("Resumen del análisis:")
     print(f"Total genes significativos: {len(classify)}")
@@ -260,6 +466,7 @@ def main():
     args = parsear_argumentos()
     filename = args.input
     output = args.output
+    gff_file = args.gff_file
     padj_threshold = args.padj_threshold
     lfc_threshold = args.lfc_threshold
     gene_col = args.columna_gene
@@ -268,9 +475,15 @@ def main():
 
     genes_tupla = load_deseq2_results(filename, gene_col, lfc_col, padj_col)
 
-    filtro = is_significant(genes_tupla, padj_threshold, lfc_threshold)
+    gff_data = load_gff(gff_file)
 
-    classify = classify_gene(filtro)
+    gff_significant_data = save_gff_data(gff_data)
+
+    merged_data = merge_data(genes_tupla, gff_significant_data)
+
+    filtro = is_significant(merged_data, padj_threshold, lfc_threshold)
+
+    classify = sorted(classify_gene(filtro))
 
     write_results(output, classify)
 
